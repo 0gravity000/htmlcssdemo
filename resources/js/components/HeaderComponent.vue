18b277fb3a901451015e62d4982b7e350f91cfe2
0gravity000 <0gravity000@gmail.com>
2020-10-12 13:37:10 +0900

    ヘッダーのタイトル文字に一覧ページへのリンクを追加
    Changes to be committed:
        modified:   resources/js/components/HeaderComponent.vue

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid mb-3" style="background-color: #e3f2fd;">
         <div class="container">
             <nav class="navbar navbar-light">
-                <span class="navbar-brand mb-0 h1">HTML CSS Demo</span>
+                <span class="navbar-brand mb-0 h1"><a href="/list">HTML CSS Demo</a></span>
                 <div>
                     <button type="button" class="btn btn-link"><a href="/">トップページへ</a></button>
                     <button type="button" class="btn btn-link"><a href="/list">一覧ページへ</a></button>
